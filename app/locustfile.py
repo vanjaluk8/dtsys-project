64e08da9fed835039593626b1d02025fb205fd90
Vanja Luk <vanja.luk@gmail.com>
2024-02-03 23:20:21 +0100

    popravljeni testovi za nove endpointove

--- app/locustfile.py
+++ app/locustfile.py
@@ -5,18 +5,18 @@ class WebsiteUser(HttpUser):
     wait_time = between(1, 2.5)
     host = "http://127.0.0.1:8000"
 
-    @task
-    def return_nasdaq_async(self):
-        self.client.get("/return_nasdaq_async")
+    @task(1)
+    def return_index_async(self):
+        self.client.get("/return_index_data?collection_name=NASDAQ")
 
-    @task
-    def return_nasdaq_sync(self):
-        self.client.get("/return_nasdaq_sync")
+    @task(1)
+    def return_index_sync(self):
+        self.client.get("/return_index_data_sync?collection_name=NASDAQ")
 
-    @task
-    def add_stock_data_async(self):
-        self.client.post("/add_stock_data_async", json={"file_name": "stocks/AMZN.json"})
-
-    @task
-    def add_stock_data_sync(self):
-        self.client.post("/add_stock_data_sync", json={"file_name": "stocks/AMZN.json"})
+    # @task
+    # def add_stock_data_async(self):
+    #     self.client.post("/add_stock_data_async", json={"file_name": "stocks/AMZN.json"})
+    #
+    # @task
+    # def add_stock_data_sync(self):
+    #     self.client.post("/add_stock_data_sync", json={"file_name": "stocks/AMZN.json"})
