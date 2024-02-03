e9ec6e2df64fe138cc0eca4f970dc84c73de96cd
Vanja Luk <vanja.luk@gmail.com>
2024-02-03 22:25:53 +0100

    dodani novi testovi

--- app/locustfile.py
+++ app/locustfile.py
@@ -1,11 +1,22 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 
 
 class WebsiteUser(HttpUser):
+    wait_time = between(1, 2.5)
+    host = "http://127.0.0.1:8000"
+
     @task
-    def list_index_value(self):
+    def return_nasdaq_async(self):
         self.client.get("/return_nasdaq_async")
 
     @task
-    def list_index_value2(self):
+    def return_nasdaq_sync(self):
         self.client.get("/return_nasdaq_sync")
+
+    @task
+    def add_stock_data_async(self):
+        self.client.post("/add_stock_data_async", json={"file_name": "stocks/AMZN.json"})
+
+    @task
+    def add_stock_data_sync(self):
+        self.client.post("/add_stock_data_sync", json={"file_name": "stocks/AMZN.json"})
