6dba5be1307288ca086fd77dcb3c8b47fb054d80
Vanja Luk <vanja.luk@gmail.com>
2024-02-04 11:02:30 +0100

    moved to rapidAPI for live stock data, added new methods for querying rapidAPI and storing to local mysql db for further visualisaton

--- app/locustfile.py
+++ app/locustfile.py
@@ -5,18 +5,25 @@ class WebsiteUser(HttpUser):
     wait_time = between(1, 2.5)
     host = "http://127.0.0.1:8000"
 
-    @task(1)
-    def return_index_async(self):
-        self.client.get("/return_index_data?collection_name=NASDAQ")
 
-    @task(1)
-    def return_index_sync(self):
-        self.client.get("/return_index_data_sync?collection_name=NASDAQ")
+    @task
+    def fetch_stock_price(self):
+        symbol = "AMZN"
+        start_date = "2023-01-01"
+        end_date = "2024-01-01"
 
-    # @task
-    # def add_stock_data_async(self):
-    #     self.client.post("/add_stock_data_async", json={"file_name": "stocks/AMZN.json"})
-    #
-    # @task
-    # def add_stock_data_sync(self):
-    #     self.client.post("/add_stock_data_sync", json={"file_name": "stocks/AMZN.json"})
+        # Define the URL with parameters
+        url = f"/stock_price/{symbol}?start_date={start_date}&end_date={end_date}"
+
+        # Make a GET request to the specified URL
+        self.client.get(url)
+
+    @task
+    def fetch_stock_info(self):
+        symbol = "AMZN"
+        self.client.get(f"/stock_info/{symbol}")
+
+    @task
+    def fetch_stock_info_sync(self):
+        symbol = "AMZN"
+        self.client.get(f"/stock_sync/{symbol}")
--- app/main.py
+++ app/main.py
@@ -1,86 +1,80 @@
-import glob
-import os
+import concurrent
+from datetime import datetime
 
+import aiohttp
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 import pandas as pd
-from pymongo import MongoClient
-from datetime import datetime
+from sqlalchemy import create_engine, Table, MetaData
 
-from models.indexes import Index
 
 app = FastAPI()
 user = 'root'
 password = 'DOCKER123!'
 
+rapidapi_key = "66e942c532msh6ff9c30c0f88dd1p1fae5djsne4e8f00e39c7"
+rapidapi_endpoint = "https://apidojo-yahoo-finance-v1.p.rapidapi.com"
 
-# date time now
-
-@app.on_event("startup")
-async def startup_event():
-    app.mongodb_client = AsyncIOMotorClient(f'mongodb://{user}:{password}@localhost:27017')
-    app.mongodb1 = app.mongodb_client['index_data']
-    app.mongodb2 = app.mongodb_client['stock_data']
-
-
-
-### ASINKRONI DIO
-@app.get("/return_index_data")
-async def return_index_data(collection_name: Index):
-    """
-        Returns all documents from the NASDAQ collection in the index_data database
-        Use symbol name: NASDAQ, SP500, QQQ
-    """
-    cursor = app.mongodb1[collection_name.value].find()
-    # convert to list
-    data = await cursor.to_list(length=10000)  # specify a length limit
-    # convert ObjectId to string
-    for item in data:
-        item['_id'] = str(item['_id'])
-    # convert to dataframe and save to csv
-    df = pd.DataFrame(data)
-    df.to_csv(f'indexes/{collection_name}.async.csv', index=False)
-
-    return data
-
-@app.post("/add_stock_data")
-async def add_stock_data(file_name: str):
-    df = pd.read_csv(f'stocks/{file_name}.csv')
-    data = df.to_dict(orient='records')
-    db = app.mongodb2
-    collection = db[file_name.split('.')[0] + '_' + datetime.now().isoformat()]
-    await collection.insert_many(data)
-
-    return {
-        "message": f"Data from {file_name} uploaded to {file_name.split('.')[0]} collection in 'stock_data' database"}
+# Assuming you have a MySQL connection URL. Adjust accordingly.
+mysql_url = "mysql+pymysql://root:DOCKER123!@127.0.0.1/stock_data"
+engine = create_engine(mysql_url)
+metadata = MetaData()
+# Assuming your_table_name is the name of your MySQL table
+symbol = None
 
 
-## SINKRONI DIO
-@app.get("/return_index_data_sync")
-def return_nasdaq_sync(collection_name: Index):
-    client = MongoClient(f'mongodb://{user}:{password}@localhost:27017')
-    db = client['index_data']
-    cursor = db[collection_name.value].find()
-    # convert to list
-    data = list(cursor)
-    # convert ObjectId to string
-    for item in data:
-        item['_id'] = str(item['_id'])
-    # convert to dataframe
-    df = pd.DataFrame(data)
-    df.to_csv(f'indexes/{collection_name}.sync.csv', index=False)
-    return data
+### RAPIDAPI METHODS
+@app.get("/stock_info/{symbol}")
+async def fetch_data(symbol: str):
+    global global_symbol
+    global_symbol = symbol
+    async with aiohttp.ClientSession() as session:
+        url = f"{rapidapi_endpoint}/stock/v2/get-summary?symbol={symbol}&region=US"
+        headers = {"X-RapidAPI-Key": rapidapi_key}
 
-@app.post("/add_stock_data_sync")
-def add_stock_data_sync(file_name: str):
-    df = pd.read_csv(f'stocks/{file_name}.csv')
-    data = df.to_dict(orient='records')
-    client = MongoClient(f'mongodb://{user}:{password}@localhost:27017')
-    db = client['stock_data']
-    collection = db[file_name.split('.')[0] + '_' + datetime.now().isoformat()]
-    collection.insert_many(data)
-    return {
-        "message": f"Data from {file_name} uploaded to {file_name.split('.')[0]} collection in 'stock_data' database"}
+        async with session.get(url, headers=headers) as response:
+            stock_data = await response.json()
+
+    return stock_data
+
+
+@app.get("/stock_price/{symbol}")
+async def fetch_price_data(symbol: str, start_date: str, end_date: str):
+
+    async with aiohttp.ClientSession() as session:
+        url = f"{rapidapi_endpoint}/stock/v2/get-historical-data?symbol={symbol}&region=US&from={start_date}&to={end_date}"
+        headers = {"X-RapidAPI-Key": rapidapi_key}
+
+        async with session.get(url, headers=headers) as response:
+            response_data = await response.json()
+            stock_data_list = response_data["prices"]
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for stock_data in stock_data_list:
+                if all(key in stock_data for key in ["date", "open", "high", "low", "close", "volume", "adjclose"]):
+                    # Convert the date from a timestamp to a datetime object
+                    stock_data["date"] = datetime.fromtimestamp(stock_data["date"])
+
+                    # Ensure the other fields are of the correct data type
+                    stock_data["open"] = float(stock_data["open"])
+                    stock_data["high"] = float(stock_data["high"])
+                    stock_data["low"] = float(stock_data["low"])
+                    stock_data["close"] = float(stock_data["close"])
+                    stock_data["volume"] = int(stock_data["volume"])
+                    stock_data["adjclose"] = float(stock_data["adjclose"])
+
+                    # Create a DataFrame from the stock data
+                    df = pd.DataFrame([stock_data])
+
+                    try:
+                        # Run the to_sql() method in a separate thread
+                        executor.submit(df.to_sql, symbol, con=engine, if_exists='append',
+                                        index=False, insert='multi')
+                    except Exception as e:
+                        print(e)
+
+    return stock_data, {"message": "Data has been successfully added to the database."}
+
 
 # if __name__ == "__main__":
 #     import uvicorn
